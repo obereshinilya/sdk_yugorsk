9732f66f5e0a1d3298333794498c6cf5abde9486
obereshinilya <87429541+obereshinilya@users.noreply.github.com>
2022-07-20 10:45:25 +0300

    До внедрения dataTables
    До внедрения dataTables

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,12 +15,11 @@ class ReportController extends Controller
     public function index(Request $request)
     {
         AdminController::log_record('Открыл документарный блок  ');//пишем в журнал
-
         return view('web.docs.reports.report_main');
     }
     public function actual_declarations(Request $request)
     {
-        AdminController::log_record('Открыл реестр актуальных декларация ПБ  ');//пишем в журнал
+        AdminController::log_record('Открыл реестр актуальных декларация ПБ ');//пишем в журнал
         $data_to_table = ActualDeclarations::orderByDesc('id')->get();
         return view('web.docs.reports.report_actual_declarations', compact('data_to_table'));
     }
